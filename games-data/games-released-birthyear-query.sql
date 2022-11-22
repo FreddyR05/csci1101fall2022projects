84eca20f03ccdcfd25c6139f8e765a3882cf31b0
SomeEdward <2012678@ljisd.com>
2022-11-22 13:23:11 -0600

    games-data

---
+++ games-data/games-released-birthyear-query.sql
@@ -0,0 +1,1 @@
+SELECT * FROM games WHERE first_release_date BETWEEN '1999-05-21 00:00:00' AND '1999-12-31 23:59:59' ORDER BY name;
---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+--INSERT INTO sandbox ( int64_value, bool_value, datetime_value) VALUES (5219980085, 1, 1973-10-01);
+
+--UPDATE sandbox SET double_value = -3.146748928 WHERE id = 149
+
+--DELETE FROM sandbox WHERE id = 71;
+
+SELECT * FROM sandbox; 
